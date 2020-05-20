1bfcafe06eb0dbd18b0312b7f48d894f8af6214a
Zhenghao Li <lizhenghao99@outlook.com>
2020-05-20 02:32:03 -0700

    static level complete

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,21 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathFX = null;
     [SerializeField] Transform parent = null;
+    [SerializeField] int scorePerHit = 1200;
+
+    ScoreBoard scoreBoard;
 
     // Start is called before the first frame update
     void Start()
     {
         Collider collider = gameObject.AddComponent<BoxCollider>();
         collider.isTrigger = false;
+
+        scoreBoard = FindObjectOfType<ScoreBoard>();
     }
 
     private void OnParticleCollision(GameObject other)
     {
+        scoreBoard.ScoreHit(scorePerHit);
         GameObject fx = Instantiate(
             deathFX, transform.position, Quaternion.identity);
         fx.transform.parent = parent;
